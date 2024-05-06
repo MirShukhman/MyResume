c06b882208e96b72b8cf5028f0937c581fb81dc9
MirShukhman <miriysha@gmail.com>
2024-05-06 12:52:56 +0300

    mobile hero

--- my-resume-front/src/comps/Header.js
+++ my-resume-front/src/comps/Header.js
@@ -16,9 +16,17 @@ const Header = () => {
                     <p id='hero-top'>
                         Hi, I am Mir
                     </p>
-                    <p id='hero-main'>
-                        Full Stack Web Developer | Software Developer
-                    </p>
+                    {window.innerWidth < 768 ? (
+                        <p id='hero-main'>
+                            Full Stack Web Developer
+                        </p>
+
+                    ) :
+                        (
+                            <p id='hero-main'>
+                                Full Stack Web Developer | Software Developer
+                            </p>
+                        )}
                 </div>
 
                 <div>
