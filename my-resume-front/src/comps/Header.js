1377832782221d59639ae2bf99c469e5962dc561
MirShukhman <miriysha@gmail.com>
2024-05-07 13:42:27 +0300

    front deploy

--- my-resume-front/src/comps/Header.js
+++ my-resume-front/src/comps/Header.js
@@ -29,9 +29,7 @@ const Header = () => {
                         )}
                 </div>
 
-                <div>
-                    <div className='hero-img'></div>
-                </div>
+
             </div>
 
         </div>
