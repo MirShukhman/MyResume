fcb900bf0b9667b50a1a302bad6a852210dceedf
MirShukhman <miriysha@gmail.com>
2024-08-07 21:45:04 +0300

    pages

--- my-resume-front/src/App.js
+++ my-resume-front/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
 
       <Routes>
-        <Route path='/MyResume' element={
+        <Route exact path='/' element={
           <>
             <Header />
 
